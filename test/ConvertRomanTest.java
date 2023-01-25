840485363706e5a4ecf8b29637d91b7703a0c4d3
Noxi37 <theo.derieux@outlook.fr>
2023-01-25 14:16:54 +0100

    Add fake test for the CI/CD deployement

---
+++ test/ConvertRomanTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ConvertRomanTest {
+
+    //assert equals true
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
+
+}
