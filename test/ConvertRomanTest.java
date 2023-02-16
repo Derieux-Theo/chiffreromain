84767a224a914db4c5b13789054bcf775044dbc0
Theo Derieux <theo.derieux@ouidou.fr>
2023-02-16 15:01:39 +0100

    Test 1 : 1 -> I

--- test/ConvertRomanTest.java
+++ test/ConvertRomanTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ConvertRomanTest {
@@ -8,6 +9,12 @@ public class ConvertRomanTest {
     @Test
     public void Test1() {
         assertTrue(true);
+    }
+
+    // convert 1 to I
+    @Test
+    public void Test2() {
+        assertEquals("I", ConvertRoman.convert(1));
     }
 
 }
